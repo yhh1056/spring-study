92996a4153231a299e393dbd4430a45e19750cb3
yhh1056 <yhh1056@naver.com>
2022-10-22 18:02:11 +0900

    feat: eventListener 트랜잭션 로그

--- event/src/main/java/com/example/reservation/ReservationEventListener.java
+++ event/src/main/java/com/example/reservation/ReservationEventListener.java
@@ -1,18 +1,24 @@
 package com.example.reservation;
 
+import com.example.common.TransactionLogger;
 import com.example.coupon.CouponUsedEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ReservationEventListener {
 
     private final ReservationService reservationService;
 
     @EventListener
     public void accept(final CouponUsedEvent couponUsedEvent) {
+        log.info("====ReservationEventListener Start====");
+        TransactionLogger.logActualTransactionActive();
         reservationService.accept(couponUsedEvent.getCouponId());
+        log.info("====ReservationEventListener Finish====");
     }
 }
--- event/src/test/java/com/example/coupon/CouponReservationCouponStatusServiceTest.java
+++ event/src/test/java/com/example/coupon/CouponReservationCouponStatusServiceTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-// 테스트코드간 의존성은 신경쓰지 않았습니다.
+// 테스트코드간 의존성은 신경쓰지 않는다.
 class CouponReservationCouponStatusServiceTest {
 
     @Autowired
@@ -28,9 +28,10 @@ class CouponReservationCouponStatusServiceTest {
     @DisplayName("쿠폰을 즉시 사용할 경우 예약의 상태가 승인으로 변경된다.")
     void useImmediately() {
         Long couponId = couponService.save("호호의 커피 쿠폰");
-        // 예약이 저장되면 WAITING 상태가 된다.
+        // 예약이 저장되면 에약은 WAITING 상태가 된다.
         Long reservationId = reservationService.save(couponId);
 
+        // 이벤트가 발행된다.
         couponService.useImmediately(couponId);
 
         // 쿠폰이 즉시 사용됐다면 예약은 ACCEPT 상태로 변경되어야 한다.
